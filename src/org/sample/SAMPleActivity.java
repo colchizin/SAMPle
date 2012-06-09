0090a0f63b6a532e50c159290a8670992308688f
nsieg <N.Siegfried@gmx.de>
2012-06-09 23:43:02 +0200

    Two activities

--- src/org/sample/SAMPleActivity.java
+++ src/org/sample/SAMPleActivity.java
@@ -1,13 +1,33 @@
 package org.sample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class SAMPleActivity extends Activity {
-    /** Called when the activity is first created. */
+public class SAMPleActivity extends Activity implements OnClickListener {
+    
+	private Button goButton;
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.start);
+        
+        goButton=(Button) findViewById(R.id.button_go);
+        goButton.setOnClickListener(this);
+        
     }
+
+	public void onClick(View arg0) {
+		
+		//Starte Schritterfassung
+		//Warte 10 Sekunden
+		
+		//Wechsle zur LaufActivity
+		startActivity(new Intent(this, WalkActivity.class));		
+	}
 }
---
+++ src/org/sample/WalkActivity.java
@@ -0,0 +1,16 @@
+package org.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class WalkActivity extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);      
+        
+    }
+	
+}
