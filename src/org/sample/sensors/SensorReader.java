c7480160b9a9ff5b84e811b1c14e4646facfb415
Sebastian Gibb <mail@sebastiangibb.de>
2012-06-09 12:44:58 +0200

    Kommentare vervollständigt

--- src/org/sample/sensors/SensorReader.java
+++ src/org/sample/sensors/SensorReader.java
@@ -23,8 +23,11 @@ public class SensorReader implements SensorEventListener {
      * Ctor
      */
     public SensorReader(Context context) {
+    	// create own context to access sensor service
     	mContext = context; 
+    	// access sensors
         mSensorManager = (SensorManager)mContext.getSystemService(Context.SENSOR_SERVICE);
+        // access only acceleration sensor
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
     
@@ -36,37 +39,51 @@ public class SensorReader implements SensorEventListener {
     }
 
     /**
-     * start event listener for for acceleration sensor
+     * start event listener for acceleration sensor
      */
 	public void start() {
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
     }
 
+    /**
+     * stop event listener for acceleration sensor
+     */
     public void stop() {
         mSensorManager.unregisterListener(this);
     }
     
-	@Override
+    /**
+     * handle sensor events
+     */
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER)
+    	
+    	// only handle acceleration events
+        if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER) {
             return;
+        }
 
+        // calculate scalar 
         float scalar = event.values[0]*event.values[0] + // x
         			   event.values[1]*event.values[1] + // y
         			   event.values[2]*event.values[2]; // z
+        
         mSensorValues.add(Float.valueOf(scalar));
 	}
 	
-	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 		// TODO
 	}
 	
+	/**
+	 *	get collected sensor scalar data 
+	 */
 	public ArrayList<Float> sensorValues() {
 		return mSensorValues;
 	}
-	
 
+	/**
+	 *	clear stored sensor data
+	 */
 	public void clear() {
 		mSensorValues.clear();
 	}
